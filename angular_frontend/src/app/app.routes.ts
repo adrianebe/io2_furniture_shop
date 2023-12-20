582ef0fc927a4701cec73c7fe7e3d2c4299a5129
Adrian Bilski <adrian.bilski2b@gmail.com>
2023-12-20 20:01:51 +0100

    angular

---
+++ angular_frontend/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { BathroomComponent } from './bathroom/bathroom.component';
+
+export const routes: Routes = [{path: 'bathroom', component: BathroomComponent}];
