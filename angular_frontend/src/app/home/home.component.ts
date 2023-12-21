18b8dfee10e03f50bd9a96a9ca273ab7747b5dd7
Adrian Bilski <adrian.bilski2b@gmail.com>
2023-12-21 02:11:44 +0100

    [ANGULAR] dodanie formularza rejestracji

--- angular_frontend/src/app/home/home.component.ts
+++ angular_frontend/src/app/home/home.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class HomeComponent {
 }
