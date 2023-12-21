5b1e2cdfec4f8e9f2bddb4506e9c55b300e2f4d4
Adrian Bilski <adrian.bilski2b@gmail.com>
2023-12-21 01:52:03 +0100

    [ANGULAR] dodanie ikon bootstrap, dodanie komponentow

--- angular_frontend/src/app/app.routes.ts
+++ angular_frontend/src/app/app.routes.ts
@@ -8,8 +8,11 @@ import { LivingRoomComponent } from './living-room/living-room.component';
 import { ContactComponent } from './contact/contact.component';
 import { CustomerServiceComponent } from './customer-service/customer-service.component';
 import { BoardsComponent } from './boards/boards.component';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [{path: 'bathroom', component: BathroomComponent},
+export const routes: Routes = [{path: 'home', component: HomeComponent},
+{path: 'bathroom', component: BathroomComponent},
 {path: 'bedroom', component: BedroomComponent},
 {path: 'kitchen', component: KitchenComponent},
 {path: 'living_room', component: LivingRoomComponent},
@@ -17,6 +20,7 @@ export const routes: Routes = [{path: 'bathroom', component: BathroomComponent},
 {path: 'login', component: LoginComponent},
 {path: 'register', component: RegisterComponent},
 {path: 'contact', component: ContactComponent},
-{path: 'customer_service', component: CustomerServiceComponent}];
+{path: 'customer_service', component: CustomerServiceComponent},
+{path: 'cart', component: CartComponent}];
 
 
