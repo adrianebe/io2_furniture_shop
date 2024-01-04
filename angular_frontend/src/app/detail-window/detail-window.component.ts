273954e686a6d83de31ed50157cbf524aa023241
Adrian Bilski <adrian.bilski2b@gmail.com>
2024-01-04 18:05:04 +0100

    [ANGULAR] podlaczenie z backendem

---
+++ angular_frontend/src/app/detail-window/detail-window.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-window',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-window.component.html',
+  styleUrl: './detail-window.component.scss'
+})
+export class DetailWindowComponent {
+
+}
--- angular_frontend/src/app/kitchen/kitchen.component.ts
+++ angular_frontend/src/app/kitchen/kitchen.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AssortmentService } from '../services/assortment.service';
+import { Assortment } from '../DTO/assortment';
 
 @Component({
   selector: 'app-kitchen',
@@ -9,4 +11,15 @@ import { Component } from '@angular/core';
 })
 export class KitchenComponent {
 
+  assortment: Assortment[];
+
+  constructor(private assortmentService: AssortmentService) {
+    this.assortment = [];
+  }
+
+  ngOnInit() {
+    this.assortmentService.getAssortment().subscribe((data: any[]) => {
+      this.assortment = data;
+    });
+  }
 }
