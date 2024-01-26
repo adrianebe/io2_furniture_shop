49dc081a9efd772e436aaad36e9ac9c3315d1c04
Debe <100838901+adrianebe@users.noreply.github.com>
2024-01-26 15:49:08 +0100

    Delete angular_frontend directory

--- angular_frontend/src/app/app.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { BathroomComponent } from './bathroom/bathroom.component';
-
-export const routes: Routes = [{path: 'bathroom', component: BathroomComponent}];
