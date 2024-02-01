4f4efda2be27e73820cda28d5cf5ed2208261e6b
Adrian Bilski <adrian.bilski2b@gmail.com>
2024-02-01 03:44:26 +0100

    [ANGULAR] raporty finansowe

--- angular_frontend/src/app/financial-reports/financial-reports.component.ts
+++ angular_frontend/src/app/financial-reports/financial-reports.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { EmployeeService } from '../services/employee.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-financial-reports',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgFor],
   templateUrl: './financial-reports.component.html',
   styleUrl: './financial-reports.component.scss'
 })
-export class FinancialReportsComponent {
+export class FinancialReportsComponent implements OnInit {
+  reportData: any = {};
+  financialReports: any[] = [];
 
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.loadFinancialReports();
+  }
+
+  generateFinancialReport(): void {
+    this.employeeService.generateFinancialReport(this.reportData).subscribe(
+      (response) => {
+        console.log('Financial report generated successfully:', response);
+        this.loadFinancialReports();
+      },
+      (error) => {
+        console.error('Error generating financial report:', error);
+      }
+    );
+  }
+
+  loadFinancialReports(): void {
+    this.employeeService.getAllFinancialReports().subscribe(
+      (data) => {
+        this.financialReports = data;
+      },
+      (error) => {
+        console.error('Error loading financial reports:', error);
+      }
+    );
+  }
 }
